94581a7bfaa3893051881dbb92c5cfe9ffeb8104
sohan <sohanprasad2003@gmail.com>
2025-01-30 19:46:54 +0530

    add border radius to dark theme button

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.getElementById("themeToggleButton").addEventListener("click", function() {
     document.body.classList.toggle("light-theme");
     document.body.classList.toggle("dark-theme");
-    // document.body.classList.toggle("font-color");
+    
     document.body.classList.toggle("label");
 });
 
-// Initially set to light theme
 document.body.classList.add("light-theme");
